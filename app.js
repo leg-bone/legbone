160e867948c6f3e033e417195338ef1542fe2a32
charlie <wct257@nyu.edu>
2021-04-17 00:37:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,16 +15,17 @@ function init() {
   const fov = 35;
   const aspect = container.clientWidth / container.clientHeight;
   const near = 0.1;
-  const far = 1000;
+  const far = 10000;
 
   //Camera setup
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.set(45, (window.innerWidth * 4)/window.innerHeight, 1, 1000 );
+  camera.position.z = 100;
 
-  const ambient = new THREE.AmbientLight(0x404040, 6);
+  const ambient = new THREE.AmbientLight(0x404040, 8);
   scene.add(ambient);
 
-  const light = new THREE.DirectionalLight(0xffffff, 6);
+  const light = new THREE.DirectionalLight(0xffffff, 8);
   light.position.set(50, 50, 100);
   scene.add(light);
   //Renderer
