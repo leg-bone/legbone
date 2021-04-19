d0a26d39fbd5b8e7b936ccdb7015daeac7e74983
charlie <wct257@nyu.edu>
2021-04-19 16:31:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -46,9 +46,9 @@ function init() {
 
 function animate() {
   requestAnimationFrame(animate);
-  object.rotation.x += (Math.random() * (.02 - .009) + .05);
-    object.rotation.z += (Math.random() * (.02 - .009) + .05);
-    object.rotation.y += (Math.random() * (.02 - .009) + .05);
+  object.rotation.x += (Math.random() * (.01 - .009) + .05);
+    object.rotation.z += (Math.random() * (.01 - .009) + .05);
+    object.rotation.y += (Math.random() * (.01 - .009) + .05);
   renderer.render(scene, camera);
 }
 
