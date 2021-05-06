b1f9b66d66561194d4e8c0d3aea0e96dd0a2a8f9
charlie <wct257@nyu.edu>
2021-05-06 14:10:29 -0400

    orbitcontrols

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let camera;
 let renderer;
 let scene;
 let object;
+var controls;
 
 function init() {
   container = document.querySelector(".scene");
@@ -17,16 +18,21 @@ function init() {
   const near = 0.1;
   const far = 10000;
 
+
   //Camera setup
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(45, (window.innerWidth * 4)/window.innerHeight, 1, 1000 );
-  camera.position.z = 1008;
+  camera.position.set(45, (window.innerWidth * 4)/window.innerHeight, 1, 10000000 );
+  camera.position.z = 2000;
+  camera.lookAt(0,0,0);
 
-  const ambient = new THREE.AmbientLight(0x404040, 8);
+
+
+
+  const ambient = new THREE.AmbientLight(0x404040, 10);
   scene.add(ambient);
 
-  const light = new THREE.DirectionalLight(0xffffff, 8);
-  light.position.set(50, 50, 100);
+  const light = new THREE.DirectionalLight(0xffffff, 100);
+  light.position.set(5000, 50, 10000);
   scene.add(light);
   //Renderer
   renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
@@ -34,21 +40,29 @@ function init() {
   renderer.setPixelRatio(window.devicePixelRatio);
 
   container.appendChild(renderer.domElement);
-
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
   //Load Model
+
+const texture = new THREE.TextureLoader().load('./3d/textures/bernoSurface_Color.jpg');
+const material = new THREE.MeshBasicMaterial({ map : texture });
+
   let loader = new THREE.GLTFLoader();
   loader.load("./borfustexHTML.gltf", function(gltf) {
     scene.add(gltf.scene);
     object = gltf.scene.children[0];
+
     animate();
   });
 }
 
 function animate() {
   requestAnimationFrame(animate);
-  object.rotation.x += (Math.random() * (.6 - .05) + .05);
-    object.rotation.z += (Math.random() * (.6 - .05) + .05);
-    object.rotation.y += (Math.random() * (.6 - .05) + .05);
+
+
+//    object.rotation.y += (Math.random() * (.0001 - .05) + .0000001);
+
+
+
   renderer.render(scene, camera);
 }
 
